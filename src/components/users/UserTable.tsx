9f7fc7479170fbbe66634e3e9e06c0010e8b774b
ShanifKn <shanifkaraden06@gmail.com>
2024-12-09 10:53:07 +0530

    small update

--- src/components/users/UserTable.tsx
+++ src/components/users/UserTable.tsx
@@ -4,19 +4,8 @@ import React, { useState } from "react";
 import { TableData } from "@/core/interfaces/data/user.interface";
 import { Check, ChevronsUpDown, UserPen } from "lucide-react";
 import { Button } from "../ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation"; // Import useRouter
 // Define the props for the component
@@ -36,9 +25,7 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
   const router = useRouter(); // Initialize useRouter
 
   const [search, setSearch] = useState("");
-  const [selectedBranch, setSelectedBranch] = useState<string | null>(
-    "All Branches"
-  );
+  const [selectedBranch, setSelectedBranch] = useState<string | null>("All Branches");
   const [currentPage, setCurrentPage] = useState(1);
   const rowsPerPage = 10;
 
@@ -47,10 +34,7 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
 
   // Pagination logic
   const totalPages = Math.ceil(filteredUsers.length / rowsPerPage);
-  const paginatedUsers = filteredUsers.slice(
-    (currentPage - 1) * rowsPerPage,
-    currentPage * rowsPerPage
-  );
+  const paginatedUsers = filteredUsers.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage);
 
   // Handle redirection to user details
   const handleRedirect = (userId: string) => {
@@ -69,59 +53,51 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
             onChange={(e) => setSearch(e.target.value)}
           />
 
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                role="combobox"
-                aria-expanded={!!selectedBranch}
-                className="w-[200px] justify-between bg-gray-200 hover:bg-gray-200"
-              >
-                {selectedBranch
-                  ? frameworks.find((fw) => fw.value === selectedBranch)
-                      ?.label || "All Branches"
-                  : "Select Branch..."}
-                <ChevronsUpDown className="opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandInput placeholder="Search branch..." className="h-9" />
-                <CommandList>
-                  <CommandEmpty>No branch found.</CommandEmpty>
-                  <CommandGroup>
-                    <CommandItem
-                      onSelect={() => setSelectedBranch("All Branches")}
-                    >
-                      All Branches
-                      <Check
-                        className={`ml-auto ${
-                          selectedBranch === "All Branches"
-                            ? "opacity-100"
-                            : "opacity-0"
-                        }`}
-                      />
-                    </CommandItem>
-                    {frameworks.map((fw) => (
-                      <CommandItem
-                        key={fw.value}
-                        onSelect={() => setSelectedBranch(fw.value)}
-                      >
-                        {fw.label}
-                        <Check
-                          className={`ml-auto ${
-                            selectedBranch === fw.value
-                              ? "opacity-100"
-                              : "opacity-0"
-                          }`}
-                        />
+          <div className="flex gap-3">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={!!selectedBranch}
+                  className="w-[200px] justify-between bg-gray-200 hover:bg-gray-200">
+                  {selectedBranch ? frameworks.find((fw) => fw.value === selectedBranch)?.label || "All Branches" : "Select Branch..."}
+                  <ChevronsUpDown className="opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput
+                    placeholder="Search branch..."
+                    className="h-9"
+                  />
+                  <CommandList>
+                    <CommandEmpty>No branch found.</CommandEmpty>
+                    <CommandGroup>
+                      <CommandItem onSelect={() => setSelectedBranch("All Branches")}>
+                        All Branches
+                        <Check className={`ml-auto ${selectedBranch === "All Branches" ? "opacity-100" : "opacity-0"}`} />
                       </CommandItem>
-                    ))}
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
+                      {frameworks.map((fw) => (
+                        <CommandItem
+                          key={fw.value}
+                          onSelect={() => setSelectedBranch(fw.value)}>
+                          {fw.label}
+                          <Check className={`ml-auto ${selectedBranch === fw.value ? "opacity-100" : "opacity-0"}`} />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+
+            <Button
+              className="text-white bg-[#006666] hover:bg-emerald-800"
+              onClick={() => router.push("/users/create")}>
+              Add User
+            </Button>
+          </div>
         </div>
 
         {/* Table */}
@@ -129,32 +105,20 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
           <table className="min-w-full table-auto border-collapse border border-gray-200 shadow-lg rounded-lg">
             <thead className="bg-gray-100">
               <tr>
-                <th className="px-4 py-2 border border-gray-200 text-left">
-                  Image
-                </th>
-                <th className="px-4 py-2 border border-gray-200 text-left">
-                  Name
-                </th>
-                <th className="px-4 py-2 border border-gray-200 text-left">
-                  Email
-                </th>
-                <th className="px-4 py-2 border border-gray-200 text-left">
-                  Role
-                </th>
-                <th className="px-4 py-2 border border-gray-200 text-left">
-                  Branch
-                </th>
-                <th className="px-4 py-2 border border-gray-200 text-center">
-                  Status
-                </th>
-                <th className="px-4 py-2 border border-gray-200 text-center">
-                  Action
-                </th>
+                <th className="px-4 py-2 border border-gray-200 text-left">Image</th>
+                <th className="px-4 py-2 border border-gray-200 text-left">Name</th>
+                <th className="px-4 py-2 border border-gray-200 text-left">Email</th>
+                <th className="px-4 py-2 border border-gray-200 text-left">Role</th>
+                <th className="px-4 py-2 border border-gray-200 text-left">Branch</th>
+                <th className="px-4 py-2 border border-gray-200 text-center">Status</th>
+                <th className="px-4 py-2 border border-gray-200 text-center">Action</th>
               </tr>
             </thead>
             <tbody>
               {paginatedUsers.map((user: any) => (
-                <tr key={user._id} className="hover:bg-gray-50">
+                <tr
+                  key={user._id}
+                  className="hover:bg-gray-50">
                   <td className="px-4 py-2 border border-gray-200">
                     {user.image ? (
                       <img
@@ -163,23 +127,13 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
                         className="w-10 h-10 rounded-full"
                       />
                     ) : (
-                      <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center text-gray-600 font-bold uppercase">
-                        {user.firstName.charAt(0)}
-                      </div>
+                      <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center text-gray-600 font-bold uppercase">{user.firstName.charAt(0)}</div>
                     )}
                   </td>
-                  <td className="px-4 py-2 border border-gray-200">
-                    {user.firstName}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-200">
-                    {user.email}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-200">
-                    {user.role}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-200">
-                    {user.branch}
-                  </td>
+                  <td className="px-4 py-2 border border-gray-200">{user.firstName}</td>
+                  <td className="px-4 py-2 border border-gray-200">{user.email}</td>
+                  <td className="px-4 py-2 border border-gray-200">{user.role}</td>
+                  <td className="px-4 py-2 border border-gray-200">{user.branch}</td>
                   <td className="px-4 py-2 border border-gray-200 text-center">
                     <label className="relative inline-flex items-center cursor-pointer">
                       <input
@@ -212,8 +166,7 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
           <Button
             className="px-4 bg-gray-200 rounded disabled:opacity-50 text-black py-2 hover:bg-gray-300"
             onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-            disabled={currentPage === 1}
-          >
+            disabled={currentPage === 1}>
             Previous
           </Button>
           <span>
@@ -222,11 +175,8 @@ const UserTable: React.FC<UserTableProps> = ({ data }) => {
 
           <Button
             className="px-4 bg-gray-200 rounded disabled:opacity-50 text-black py-2 hover:bg-gray-300"
-            onClick={() =>
-              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-            }
-            disabled={currentPage === totalPages}
-          >
+            onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+            disabled={currentPage === totalPages}>
             Next
           </Button>
         </div>
